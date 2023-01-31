b812631ed1e888b78ebc25c8b62c9c03c7b62f8d
Aldito8 <>
2023-01-31 16:32:59 +0700

    Update Pet.kt

--- app/src/main/java/com/example/fafafapet/Pet.kt
+++ app/src/main/java/com/example/fafafapet/Pet.kt
@@ -4,9 +4,3 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-class Pet : Parcelable{
-    var id : Int = 0
-    var nama : String = ""
-    var no_hp : String = ""
-    var weight : String = ""
-}
